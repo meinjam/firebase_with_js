b54d83dedf749e385d9a6a44f69973ebeb8b68f7
meinjam <injam.bd.jsr@gmail.com>
2020-04-22 18:28:22 +0600

    firebase database and read data completed

---
+++ app.js
@@ -0,0 +1,25 @@
+const list = document.querySelector("ul");
+
+const addRecipe = (recipe) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+    <li>
+        <div>${recipe.title}</div>
+        <div>${time}</div>
+    </li>
+    `;
+  list.innerHTML += html;
+};
+
+db.collection("recepies")
+  .get()
+  .then((snapshot) => {
+    // when we have the data
+    snapshot.docs.forEach((doc) => {
+      // console.log(doc.data());
+      addRecipe(doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
